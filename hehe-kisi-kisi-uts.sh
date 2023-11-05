51fa4b11d853765b2510e3576aaf9fffb3e5160c
Fajar Hardeka <63125400+fajarhardeka@users.noreply.github.com>
2023-11-05 20:35:17 +0700

    Create hehe-kisi-kisi-uts.sh
    sedot gan

---
+++ hehe-kisi-kisi-uts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ea="Kisi-kisi UTS Sistem Operasi STB 23-1 \n"
+echo $ea
+printf "1. CPU Scheduling.\n"
+printf "2. Threading.\n"
+printf "3. Distribution or Parallel.\n"
+printf "4. FCFS.\n"
+
+link="https://drive.google.com/drive/folders/1YGuyEUctgD-etuMLAYEhbCHXH9XYv6sPlCe5sxVJeFAaNFAJIZOFRCDyFzsAGjH3wBwYGCUr?usp=drive_link"
+echo $link
